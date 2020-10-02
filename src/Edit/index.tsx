b35bafda8031b8446d7025fa2b753370a7355361
sichoi <clogic@toss.im>
2020-10-02 22:19:36 +0900

    기본적인 packages 셋업

---
+++ src/Edit/index.tsx
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+
+import { jsx, css } from "@emotion/core";
+import React from "react";
+
+interface Props {}
+
+export const Edit = (props: Props) => {
+  return <div>Hi!</div>;
+};
